48c269339c02e52da0a8219fd57ce6d19bb5c7fa
Joaquín Vega <109048969+sadaga1104@users.noreply.github.com>
2023-11-26 20:18:25 -0500

    Add files via upload

---
+++ base-de-preguntas.js
@@ -0,0 +1,91 @@
+let baseDePreguntas = [
+    {
+        pregunta: "¿Quién pintó la mona lisa?",
+        ayuda: "También pintó la última cena",
+        ayudaImg: "https://i.ibb.co/bzDpFYM/web3-the-last-supper-public-domain.jpg",
+        imagen: "https://i.ibb.co/9TH5d9h/mona-lisa.jpg",
+        respuesta: "Leonardo Da Vinci",
+        distractores: ["Picasso", "Beethoven", "Miguel Ángel"]
+    },
+
+    {
+        pregunta: "¿Cuál es la madre de todas las ciencias?",
+        ayuda: "Uno de sus grandes representantes fue René Descartes",
+        ayudaImg: "https://i.ibb.co/4NgXfB9/Rene-Descartes.jpg",
+        imagen: "https://i.ibb.co/qrHdMXL/Socrates.jpg",
+        respuesta: "La Filosofía",
+        distractores: ["La Física", "La Geología", "La Bilogía"]
+    },
+
+    {
+        pregunta: "¿En cuál club de futbol juega Luis Díaz?",
+        ayuda: "Es un club de Inglaterra",
+        ayudaImg: "https://i.ibb.co/Jd2qbGV/equipos-ingleses.webp",
+        imagen: "https://i.ibb.co/h1sLcqG/lo-quiere-barcelona-esto-dicen-en-liverpool-sobre-presunto-interes-en-luis-diaz.webp",
+        respuesta: "Liverpool",
+        distractores: ["El América", "Arsenal", "Barcelona FC"]
+    },
+
+    {
+        pregunta: "¿A cuánto equivale el número pi?",
+        ayuda: "Se emplea para calcular la circunferencia de un círculo",
+        ayudaImg: "https://i.ibb.co/BP9Yd9G/Formula-del-perimetro-de-un-circulo.webp",
+        imagen: "https://i.ibb.co/LzShBm4/numero-pi-2.jpg",
+        respuesta: "3.1416",
+        distractores: ["666", "1981", "1416.3"]
+    },
+
+    {
+        pregunta: "¿Cuántos departamentos tiene Colombia?",
+        ayuda: "El número es igual al cuadrado de 6 menos el doble de dos",
+        ayudaImg: "https://i.ibb.co/R6wgD3y/map-colombia.webp",
+        imagen: "https://i.ibb.co/rvBhC3m/images-colombia.jpg",
+        respuesta: "32",
+        distractores: ["10", "33", "42"]
+    },
+
+    {
+        pregunta: "¿Quién descubrió la gravedad?",
+        ayuda: "Su apellido es el mismo de las unidades de fuerza",
+        ayudaImg: "https://i.ibb.co/fDPJC5n/fuerza-1-e1562381271157-800x400.png",
+        imagen: "https://i.ibb.co/5KZNyM6/fuerza-de-gravedad1-e1537907491241.jpg",
+        respuesta: "Isaac Newton",
+        distractores: ["Nicolás Copérnico", "Albert Einstein", "Tomas Edison"]
+    },
+
+    {
+        pregunta: "¿Cuántos años tiene un lustro?",
+        ayuda: "Un siglo tiene veinte",
+        ayudaImg: "https://i.ibb.co/Dg4cFV7/siglo.webp",
+        imagen: "https://i.ibb.co/ByFxjhB/anos-que-tiene-un-lustro.jpg",
+        respuesta: "5",
+        distractores: ["25", "Infinitos", "500"]
+    },
+
+    {
+        pregunta: "¿Hace cuanto se extinguieron los dinosaurios?",
+        ayuda: "Es un número par",
+        ayudaImg: "https://i.ibb.co/XbyFvWF/fosil2.webp",
+        imagen: "https://i.ibb.co/5sLS687/teorias-de-la-extincion-de-lo-dinosaurios-1113-0-600.jpg",
+        respuesta: "66 millones de años",
+        distractores: ["1000 años", "Ayer", "33 millones de años"]
+    },
+
+    {
+        pregunta: "¿Qué es un palíndromo?",
+        ayuda: "<Reconocer> es un ejemplo",
+        ayudaImg: "https://i.ibb.co/VSgqTkh/Reconocer.png",
+        imagen: "https://i.ibb.co/Jm3Tb1g/18a35-66b53-palindromos.jpg",
+        respuesta: "Palabra que se lee igual de izquierda a derecha que de derecha a izquierda.",
+        distractores: ["Nada", "Un árbol", "Un animal terrestre"]
+    },
+
+    {
+        pregunta: "¿Quién escribió cien años de soledad?",
+        ayuda: "Colombiano ganador del premio Nobel",
+        ayudaImg: "https://i.ibb.co/hVn2Zr5/nobel-medaille.jpg",
+        imagen: "https://i.ibb.co/WHV62Z6/cien-anos-de-soledad-resumen-corto-3845-600-square.jpg",
+        respuesta: "Gabriel García Márquez",
+        distractores: ["José Eustasio Rivera", "Noam Chomsky", "Jorge Isaacs"]
+    },
+];
